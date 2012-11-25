1d14a54ddf504b59fb6a341c00fb5acfd68894c3
Stefan (nepo) <StNepomuk@web.de>
2012-11-25 15:43:43 +0100

    added build scripts and initial configuration

---
+++ ApplicationAppApi/src/edu/hm/hs/application/api/object/AbstractApiObject.java
@@ -0,0 +1,15 @@
+package edu.hm.hs.application.api.object;
+
+import edu.hm.basic.object.AbstractBasicObject;
+
+/**
+ * Abstrakte Basisklasse fuer alle ApplicationApp API Objekte.
+ * 
+ * @author Stefan Wörner
+ */
+public abstract class AbstractApiObject extends AbstractBasicObject
+{
+
+	private static final long serialVersionUID = 989098402512416096L;
+
+}
---
+++ ApplicationAppApi/src/edu/hm/hs/application/api/object/AbstractRessourceObject.java
@@ -0,0 +1,13 @@
+package edu.hm.hs.application.api.object;
+
+/**
+ * Abstrakte Basisklasse fuer alle ApplicationApp Ressource Objekte.
+ * 
+ * @author Stefan Wörner
+ */
+public abstract class AbstractRessourceObject extends AbstractApiObject
+{
+
+	private static final long serialVersionUID = 5290752638604542677L;
+
+}
