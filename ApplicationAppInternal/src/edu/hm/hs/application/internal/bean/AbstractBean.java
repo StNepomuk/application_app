3cc52853782cbda491dda31344367c5a2ef12644
Stefan (nepo) <StNepomuk@web.de>
2012-11-25 15:53:01 +0100

    added base classes and fixed classpath

---
+++ ApplicationAppInternal/src/edu/hm/hs/application/internal/bean/AbstractBean.java
@@ -0,0 +1,11 @@
+package edu.hm.hs.application.internal.bean;
+
+/**
+ * Abstrakte Basisklasse für alle Beans der ApplicationApp Server Anteile.
+ * 
+ * @author Stefan Wörner
+ */
+public abstract class AbstractBean
+{
+
+}
